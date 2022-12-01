358b9079f5324ad0e4e3a3ee43862641c7e50cea
Van Pham <vanhongpham01@gmail.com>
2022-12-01 01:40:10 -0500

    partially tested finished strat agent

--- agents/testagent.py
+++ agents/testagent.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import copy
 class MCnodes: # check https://github.com/hayoung-kim/mcts-tic-tac-toe/blob/master/VanilaMCTS.py
         def __init__(self,my_pos,barrier_dir,adv_pos,root=None,value=0,n=0,chess_board=None,visited=0):
             """
@@ -13,13 +14,15 @@ class MCnodes: # check https://github.com/hayoung-kim/mcts-tic-tac-toe/blob/mast
             self.my_pos=my_pos
             self.adv_pos=adv_pos
             self.moves=((-1, 0), (0, 1), (1, 0), (0, -1))
+            self.opposites={0: 2, 1: 3, 2: 0, 3: 1}
             self.children=[]
             self.visited=visited
             # if tree==None:
             #     self.tree=self.create_tree()
+        def __lt__(self, other):
+            return self.value < other.value
 
         def is_valid_child(self,move): #taken from world.py file
-            print("checking ",move)
             (r,c),d=move
             if self.chess_board[r, c, d]:
                 return False
@@ -62,18 +65,29 @@ class MCnodes: # check https://github.com/hayoung-kim/mcts-tic-tac-toe/blob/mast
             Generate possible children nodes (valid moves) from a node
             From deduction, for k max steps, the "reachable" region of a node is (ignoring boundary of board) a diamnod shaped bounding with vertices k-step away from the node
             """
+            
+            def set_barrier(board,pos, dir):
+                r,c=pos
+                # Set the barrier to True
+                board[r, c, dir] = True
+                # Set the opposite barrier to True
+                moves=((-1, 0), (0, 1), (1, 0), (0, -1))
+                opposites={0: 2, 1: 3, 2: 0, 3: 1}
+                move = moves[dir]
+                board[r + move[0], c + move[1], opposites[dir]] = True
+                return board
             children=[]
             # logger.info(f"Generate children for {self.move}")
             M=len(self.chess_board) #chess board size
             k=(M+1)//2 #K as defined in the pdf
-            moves=[] 
+            ms=[] 
             #generate possible moves
             for i in range(k+1):
                 for j in range (k-i+1):
                     if (i,j)==(0,0): #can't not move
                         continue
-                    moves.extend([(i,j),(-i,j),(i,-j),(-i,-j)])
-            for m in moves:
+                    ms.extend([(i,j),(-i,j),(i,-j),(-i,-j)])
+            for m in ms:
                 r=self.my_pos[0]+m[0]
                 c=self.my_pos[1]+m[1]
                 if r<0 or c<0 or r>=M or c>=M:
@@ -85,10 +99,12 @@ class MCnodes: # check https://github.com/hayoung-kim/mcts-tic-tac-toe/blob/mast
                     if self.is_valid_child((m,d)):
                         children.append((m,d))
             for c in children:
+                board=copy.deepcopy(self.chess_board)
                 m,d=c
-                child=MCnodes(m,d,self.adv_pos,root=self,chess_board=self.chess_board)
+                child=MCnodes(m,d,self.adv_pos,root=self,chess_board=set_barrier(board,m,d))
                 self.children.append(child)
             return None
+            
         
         # def selection(self):
         #     """
@@ -111,7 +127,92 @@ class MCnodes: # check https://github.com/hayoung-kim/mcts-tic-tac-toe/blob/mast
         #             max_uct=c_uct
         #             max_child=c
         #     return max_child
+        def check_endgame(self):
+            """
+            Check if the game ends and compute the current score of the agents.
+
+            Returns
+            -------
+            is_endgame : bool
+                Whether the game ends.
+            player_1_score : int
+                The score of player 1.
+            player_2_score : int
+                The score of player 2.
+            """
+            # Union-Find
+            board_size=len(self.chess_board)
+            father = dict()
+            for r in range(board_size):
+                for c in range(board_size):
+                    father[(r, c)] = (r, c)
+
+            def find(pos):
+                if father[pos] != pos:
+                    father[pos] = find(father[pos])
+                return father[pos]
+
+            def union(pos1, pos2):
+                father[pos1] = pos2
+
+            for r in range(board_size):
+                for c in range(board_size):
+                    for dir, move in enumerate(
+                        self.moves[1:3]
+                    ):  # Only check down and right
+                        if self.chess_board[r, c, dir + 1]:
+                            continue
+                        pos_a = find((r, c))
+                        pos_b = find((r + move[0], c + move[1]))
+                        if pos_a != pos_b:
+                            union(pos_a, pos_b)
+
+            for r in range(board_size):
+                for c in range(board_size):
+                    find((r, c))
+            my_r = find(tuple(self.my_pos))
+            adv_r = find(tuple(self.adv_pos))
+            my_score = list(father.values()).count(my_r)
+            adv_score = list(father.values()).count(adv_r)
+            if my_r == adv_r:
+                return False, my_score, adv_score
+            return True, my_score, adv_score
+        def distance(self,child):
+            """
+            determine how far the move is to the oponent, scaled to a number from 0 to 1 by divided by the maximum steps can be taken to reach oponent
+            """
+            ra,ca=self.adv_pos
+            rc,cc=child.my_pos
+            return (abs(ra-rc)+abs(ca-cc)-1)/((len(self.chess_board)-1)*2)
         
+        def barrier(self,child):
+            """
+            determine if the barrier is towards the oponent (0 if not, 1 if yes)
+            example: if child's barrier is at 0 (up), return 1 if the oponent is above the child (ra<rc)
+            """
+            ra,ca=self.adv_pos
+            rc,cc=child.my_pos
+            if child.barrier_dir==0:
+                    if ra<rc: return 1
+            if child.barrier_dir==1:
+                    if ca>cc: return 1
+            if child.barrier_dir==2:
+                    if ra>rc: return 1
+            if child.barrier_dir==3:
+                    if ca<cc: return 1
+            return 0
+                
+        def center(self,child):
+            """
+            calculate distance from the child to the center. We will favour children who are closer to the center (guarantee at least a draw)
+            we will use pythagoras theorem. Then scale this value to a number from 0 to 1 by divide with the furthest distance from center.
+            """
+            r,c=child.my_pos
+            n=len(self.chess_board)
+            center=math.floor(n/2)
+            dist=math.sqrt(pow((r-center),2) + pow((c-center),2))
+            return dist/math.sqrt(pow((0-center),2) + pow((0-center),2))
+
         def assign_val(self):
             """
             assign values for children based on heuristics and weights
@@ -119,94 +220,7 @@ class MCnodes: # check https://github.com/hayoung-kim/mcts-tic-tac-toe/blob/mast
                 None,
                 if return a child -> choose it right away (winning move)
             """
-        
-            def closeness(self,child):
-                """
-                determine how close the move is to the oponent
-                """
-                ra,ca=self.adv_pos
-                rc,cc=child.my_pos
-                return (abs(ra-rc)+abs(ca-cc))
             
-            def barrier(self,child):
-                """
-                determine if the barrier is towards the oponent (0 if not, 1 if yes)
-                example: if child's barrier is at 0 (up), return 1 if the oponent is above the child (ra<rc)
-                """
-                ra,ca=self.adv_pos
-                rc,cc=child.my_pos
-                if child.barrier_dir==0:
-                        if ra<rc: return 1
-                if child.barrier_dir==1:
-                        if ca>cc: return 1
-                if child.barrier_dir==2:
-                        if ra>rc: return 1
-                if child.barrier_dir==3:
-                        if ca<cc: return 1
-                return 0
-                    
-            def center(self,child):
-                """
-                calculate distance from the child to the center. We will favour children who are closer to the center (guarantee at least a draw)
-                we will use pythagoras theorem 
-                """
-                r,c=child.my_pos
-                n=len(self.chessboard)
-                center=math.floor(n/2)
-                dist=math.sqrt((r-center)^2 + (c-center)^2)
-                return dist
-            
-            def check_endgame(self):
-                """
-                Check if the game ends and compute the current score of the agents.
-
-                Returns
-                -------
-                is_endgame : bool
-                    Whether the game ends.
-                player_1_score : int
-                    The score of player 1.
-                player_2_score : int
-                    The score of player 2.
-                """
-                # Union-Find
-                board_size=len(self.chessboard)
-                father = dict()
-                for r in range(board_size):
-                    for c in range(board_size):
-                        father[(r, c)] = (r, c)
-
-                def find(pos):
-                    if father[pos] != pos:
-                        father[pos] = find(father[pos])
-                    return father[pos]
-
-                def union(pos1, pos2):
-                    father[pos1] = pos2
-
-                for r in range(board_size):
-                    for c in range(board_size):
-                        for dir, move in enumerate(
-                            self.moves[1:3]
-                        ):  # Only check down and right
-                            if self.chess_board[r, c, dir + 1]:
-                                continue
-                            pos_a = find((r, c))
-                            pos_b = find((r + move[0], c + move[1]))
-                            if pos_a != pos_b:
-                                union(pos_a, pos_b)
-
-                for r in range(board_size):
-                    for c in range(board_size):
-                        find((r, c))
-                my_r = find(tuple(self.my_pos))
-                adv_r = find(tuple(self.adv_pos))
-                my_score = list(father.values()).count(my_r)
-                adv_score = list(father.values()).count(adv_r)
-                if my_r == adv_r:
-                    return False, my_score, adv_score
-                return True, my_score, adv_score
-
 
             for child in self.children:
                 is_end_game,c,ad=child.check_endgame() #check whether or not game ended after move
@@ -215,10 +229,13 @@ class MCnodes: # check https://github.com/hayoung-kim/mcts-tic-tac-toe/blob/mast
                         return child #return child that make us win
                     if c<=ad:
                         self.children.remove(child)      #remove all child that make us loose          
-                h1=self.closeness(child)
+                h1=self.distance(child)
                 h2=self.barrier(child)
                 h3=self.center(child)
+                value=2*h2-3*h1-h3 #calculate the total value by weighted heuristics
+                child.value=value
             return None
+
 
 
 
@@ -230,11 +247,16 @@ def main():
     c[:, 0, 3] = True
     c[-1, :, 2] = True
     c[:, -1, 1] = True
-    c[:, -1, 1] = True
-    # c[0,0,2]=True
-    n1=MCnodes((0,0),2,(1,1),0,0,chess_board=c)
+
+
+    n1=MCnodes(my_pos=(1,3),barrier_dir=None,adv_pos=(3,2),value=0,chess_board=c)
     n1.get_children()
-
-
-
+    
+    n1.assign_val()
+    print(n1.children.sort(key=lambda x: x.value))
+    l=[]
+    n1.children.sort(key=lambda x: x.value)
+    for s in n1.children:
+        l.append((s.my_pos,s.barrier_dir,s.value))
+    print(l)
 main()
